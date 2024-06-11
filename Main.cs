15e0db5d3e10af7f3ce814600cffc5e810a58654
ESCAPEPOD <ESCAPEPOD@OUTCAST>
2024-06-11 18:36:54 +0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// camera behavior main controller, attach to MAIN CAMERA
+namespace CameraBehavior
+{
+    class Camera
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
+
+
